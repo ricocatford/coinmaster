5dcb638f510da35017a854252da5820ff22bf646
ricocatford <rico.albarenque@gmail.com>
2024-10-07 20:48:58 +0200

    feat(cryptocurrencies): add table with assets

--- src/components/Cryptocurrencies.tsx
+++ src/components/Cryptocurrencies.tsx
@@ -1,38 +1,53 @@
 "use client";
 
+import formatAssets from "@/lib/formatAssets";
 import useSWR from "swr";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export const Cryptocurrencies = () => {
-    const {
-        data: assets,
-        error,
-        isLoading,
-    } = useSWR("https://api.coincap.io/v2/assets", fetcher);
-
-    console.log(assets);
+    const { data, error, isLoading } = useSWR(
+        "https://api.coincap.io/v2/assets",
+        fetcher
+    );
 
     if (error) return <p>Failed to fetch assets.</p>;
 
     if (isLoading) return <p>Loading assets...</p>;
 
+    const assets = formatAssets(data.data);
+
     return (
-        <>
+        <section>
+            <h2>Cryptocurrencies</h2>
             <table>
-                <tr>
-                    <th>Rank #</th>
-                    <th>Name</th>
-                    <th>Symbol</th>
-                </tr>
-                {assets.data.map((asset: any) => (
-                    <tr key={asset.id}>
-                        <td>{asset.rank}</td>
-                        <td>{asset.name}</td>
-                        <td>{asset.symbol}</td>
+                <thead>
+                    <tr>
+                        <th># Rank</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Supply</th>
+                        <th>Market Cap</th>
+                        <th>Volume (24hr)</th>
+                        <th>Change % (24hr)</th>
                     </tr>
-                ))}
+                </thead>
+                <tbody>
+                    {assets.map((asset: any) => (
+                        <tr key={asset.id}>
+                            <td>{asset.rank}</td>
+                            <td>
+                                {asset.name} ({asset.symbol})
+                            </td>
+                            <td>{asset.price}</td>
+                            <td>{asset.supply}</td>
+                            <td>{asset.marketCap}</td>
+                            <td>{asset.volume24Hr}</td>
+                            <td>{asset.changePercent24Hr}%</td>
+                        </tr>
+                    ))}
+                </tbody>
             </table>
-        </>
+        </section>
     );
 };
