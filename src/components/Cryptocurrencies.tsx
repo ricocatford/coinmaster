62d4c1ffd59afba27b0457d9ecea35f1a2191a97
ricocatford <rico.albarenque@gmail.com>
2024-10-09 20:55:29 +0200

    fix(cryptocurrencies): remove unused imports and check assets are not undefined

--- src/components/Cryptocurrencies.tsx
+++ src/components/Cryptocurrencies.tsx
@@ -1,12 +1,8 @@
 "use client";
 
 import styles from "@/assets/styles/components/Cryptocurrencies.module.css";
-import assetsHeadings from "@/assets/data/assetsHeadings.json";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { useDataFetch } from "@/hooks/useDataFetch";
 import { Table } from "./Table";
-import Assets from "@/types/assets";
 
 export const Cryptocurrencies = () => {
     const { assets, error, isLoading } = useDataFetch();
@@ -18,7 +14,7 @@ export const Cryptocurrencies = () => {
     return (
         <section className={styles.container}>
             <h2 className={styles.heading}>Cryptocurrencies</h2>
-            <Table assets={assets} />
+            {assets && <Table assets={assets} />}
         </section>
     );
 };
