c728509b1342f7d9100ea15a1a8f0e4808c4d10f
ricocatford <rico.albarenque@gmail.com>
2024-10-11 00:38:22 +0200

    feat(tracker icon): conditionally render different icons if asset on tracker

--- src/components/AddToTrackerButton.tsx
+++ src/components/AddToTrackerButton.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as faStarFilled } from "@fortawesome/free-solid-svg-icons";
+import { faStar } from "@fortawesome/free-regular-svg-icons";
 import { GlobalStore } from "@/stores/GlobalStore";
 import { AssetId } from "@/types/assetId";
 import { useGlobal } from "@/hooks/global-store/useGlobal";
+import styles from "@/assets/styles/components/Cryptocurrencies.module.css";
 
 export const AddToTrackerButton = ({
     id,
@@ -13,12 +15,18 @@ export const AddToTrackerButton = ({
 }): React.JSX.Element => {
     const { store }: { store: GlobalStore } = useGlobal();
 
+    const isAssetTracked = store?.tracker.includes(id);
+
     return (
-        <span>
-            <FontAwesomeIcon
-                icon={faStar}
-                onClick={() => store.trackAsset(id)}
-            />
+        <span onClick={() => store.handleAsset(id)}>
+            {!isAssetTracked ? (
+                <FontAwesomeIcon icon={faStar} className={styles.trackerIcon} />
+            ) : (
+                <FontAwesomeIcon
+                    icon={faStarFilled}
+                    className={styles.trackerIcon}
+                />
+            )}
         </span>
     );
 };
