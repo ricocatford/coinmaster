a5c467a84f1134e31f1fe18f1681284308e27f51
ricocatford <rico.albarenque@gmail.com>
2024-10-11 00:37:46 +0200

    feat(store): add function for handling asset

--- src/stores/GlobalStore.ts
+++ src/stores/GlobalStore.ts
@@ -7,7 +7,7 @@ export type GlobalState = {
 };
 
 export type GlobalActions = {
-    trackAsset: (assetId: AssetId) => void;
+    handleAsset: (assetId: AssetId) => void;
     resetTracker: () => void;
 };
 
@@ -26,9 +26,14 @@ export const createGlobalStore = (initState: GlobalState = defaultInitState) => 
         persist(
             (set) => ({
                 ...initState,
-                trackAsset: (assetId: AssetId) => set((state) => {
-                    // const existingAssetId: number = state.tracker.findIndex((existingAsset) => existingAsset.id === assetId);
-                    return { tracker: [...state.tracker, assetId] }
+                handleAsset: (assetId: AssetId) => set((state) => {
+                    const existingAssetId: number = state.tracker.findIndex((existingAsset) => existingAsset === assetId);
+                    if (existingAssetId === -1) {
+                        return { tracker: [...state.tracker, assetId] }
+                    }
+
+                    const updatedTracker = state.tracker.filter((asset) => asset != assetId);
+                    return { tracker: updatedTracker }
                 }),
                 resetTracker: () => set(() => {
                     return { tracker: [] }
