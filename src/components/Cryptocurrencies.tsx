afd231848bcc5e8c1a4e34201937571bb0a39ff1
ricocatford <rico.albarenque@gmail.com>
2024-10-17 20:32:13 +0200

    fix(cryptocurrencies): add types for fetch response

--- src/components/Cryptocurrencies.tsx
+++ src/components/Cryptocurrencies.tsx
@@ -1,20 +1,27 @@
 "use client";
 
 import styles from "@/assets/styles/components/Cryptocurrencies.module.css";
-import { useFetchAllAssets } from "@/hooks/useFetchAllAssets";
 import { Table } from "./Table";
+import { useFetchAllAssets } from "@/hooks/useFetchAllAssets";
+import Asset from "@/types/asset";
 
 export const Cryptocurrencies = (): React.JSX.Element => {
-    const { assets, error, isLoading } = useFetchAllAssets();
+    const {
+        assets,
+        error,
+        isLoading,
+    }: { assets?: Asset[]; error: any; isLoading: boolean } =
+        useFetchAllAssets();
 
     if (error) return <p>Failed to fetch assets.</p>;
-
     if (isLoading) return <p>Loading assets...</p>;
 
     return (
-        <section className={styles.container}>
-            <h2 className={styles.heading}>Cryptocurrencies</h2>
-            {assets && <Table assets={assets} />}
-        </section>
+        <>
+            <section className={styles.container}>
+                <h2 className={styles.heading}>Cryptocurrencies</h2>
+                {assets && <Table assets={assets} />}
+            </section>
+        </>
     );
 };
