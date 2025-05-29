e87a1516bd655b02c4d8a9c2cd2027063ad29b7d
ricocatford <rico.albarenque@gmail.com>
2025-05-29 19:41:26 +0200

    feat(eslint): add eslint config and prettier, also fix minor issues on api

--- src/app/api/asset/[id]/route.ts
+++ src/app/api/asset/[id]/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from 'next/server';
-import "dotenv/config"
+import { NextResponse } from "next/server";
+import "dotenv/config";
 
 export async function GET(request: Request, { params }: { params: { id: string } }) {
     const { id } = params;
@@ -9,13 +9,13 @@ export async function GET(request: Request, { params }: { params: { id: string }
         const res = await fetch(`https://rest.coincap.io/v3/assets/${id}`, {
             headers: {
                 Authorization: `Bearer ${API_KEY}`,
-                Accept: 'application/json',
+                Accept: "application/json",
             },
         });
 
         const data = await res.json();
         return NextResponse.json(data);
     } catch {
-        return NextResponse.json({ error: 'Failed to fetch asset' }, { status: 500 });
+        return NextResponse.json({ error: "Failed to fetch asset" }, { status: 500 });
     }
 }
--- src/app/api/assets/route.ts
+++ src/app/api/assets/route.ts
@@ -1,20 +1,20 @@
-import { NextResponse } from 'next/server';
-import "dotenv/config"
+import { NextResponse } from "next/server";
+import "dotenv/config";
 
 export async function GET() {
     const API_KEY = process.env.API_KEY;
 
     try {
-        const res = await fetch('https://rest.coincap.io/v3/assets', {
+        const res = await fetch("https://rest.coincap.io/v3/assets", {
             headers: {
                 Authorization: `Bearer ${API_KEY}`,
-                Accept: 'application/json',
+                Accept: "application/json",
             },
         });
 
         const data = await res.json();
         return NextResponse.json(data);
     } catch {
-        return NextResponse.json({ error: 'Failed to fetch assets' }, { status: 500 });
+        return NextResponse.json({ error: "Failed to fetch assets" }, { status: 500 });
     }
 }
